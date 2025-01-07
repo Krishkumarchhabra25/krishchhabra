c6ada29b097fc1f705c521c818be489b1fb1053a
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2025-01-07 22:46:34 +0530

    images updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,37 +1,43 @@
 import { ButtonOutline, ButtonPrimary } from "./Button"
 
+import image4 from '../assets/images/image4.jpg';
+import hero2 from '../assets/images/hero2.png';
+
 const Hero = () => {
   return (
-     <section className="pt-28 lg:pt-36" id="home">
-        <div className="container lg:grid lg:grid-cols-2  items-center lg:gap-10">
-          <div>
-            <div className="flex items-center gap-3">
-              <figure className="img-box w-9 h-9 rounded-lg">
-                <img width={40} height={40}  src="/src/assets/images/image4.jpg" alt="krish chhabra"  className="img-cover"/>
-              </figure>
-              <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
-                <span className="relative w-2 h-2 rounded-full bg-emerald-400">
-                  <span className="absolute inset-0  rounded-full bg-emerald-400 animate-ping"></span>
-                </span>
-                Available for work
-              </div>
-            </div> 
-
-            <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">Building Dynamic Website and Native Apps for the Future</h2>
-            <div className="flex items-center gap-3">
-               <ButtonPrimary label="Download Cv" icon="download" />
-                <ButtonOutline href="#about" label="Scroll down" icon="arrow_downward" />
+    <section className="pt-28 lg:pt-36" id="home">
+      <div className="container lg:grid lg:grid-cols-2  items-center lg:gap-10">
+        <div>
+          <div className="flex items-center gap-3">
+            <figure className="img-box w-9 h-9 rounded-lg">
+              <img width={40} height={40} src={image4} alt="krish chhabra" className="img-cover" />
+            </figure>
+            <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+              <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                <span className="absolute inset-0  rounded-full bg-emerald-400 animate-ping"></span>
+              </span>
+              Available for work
             </div>
           </div>
 
-          <div className="hidden lg:block">
-            <figure className="w-fullmax-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden" >
-               <img width={656} height={800} src="/src/assets/images/hero2.png" alt="krish-banner" className="w-full" />
-            </figure>
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+            Building Dynamic Website and Native Apps for the Future
+          </h2>
+          <div className="flex items-center gap-3">
+            <ButtonPrimary label="Download Cv" icon="download" />
+            <ButtonOutline href="#about" label="Scroll down" icon="arrow_downward" />
           </div>
         </div>
-     </section>
-  )
-}
+
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+            <img width={656} height={800} src={hero2} alt="krish-banner" className="w-full" />
+          </figure>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 
 export default Hero
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,43 +1,51 @@
 import ProjectCard from "./ProjectCard";
 
+import project1 from '../assets/images/project-1.jpg';
+import project2 from '../assets/images/project-2.jpg';
+import project3 from '../assets/images/project-3.jpg';
+import project4 from '../assets/images/project-4.jpg';
+import project5 from '../assets/images/project-5.jpg';
+import project6 from '../assets/images/project-6.jpg';
+
 const works = [
-    {
-      imgSrc: '/src/assets/images/project-1.jpg',
-      title: 'Full stack music app',
-      tags: ['API', 'MVC', 'Development'],
-      projectLink:   'https://github.com/Krishkumarchhabra25'
-    },
-    {
-      imgSrc: '/src/assets/images/project-2.jpg',
-      title: 'Free stock photo app',
-      tags: ['API', 'SPA'],
-      projectLink: 'https://github.com/Krishkumarchhabra25'
-    },
-    {
-      imgSrc: '/src/assets/images/project-3.jpg',
-      title: 'Recipe app',
-      tags: ['Development', 'API'],
-      projectLink: ''
-    },
-    {
-      imgSrc: '/src/assets/images/project-4.jpg',
-      title: 'Real state website',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/Krishkumarchhabra25'
-    },
-    {
-      imgSrc: '/src/assets/images/project-5.jpg',
-      title: 'eCommerce website',
-      tags: ['eCommerce', 'Development'],
-      projectLink: 'https://github.com/Krishkumarchhabra25'
-    },
-    {
-      imgSrc: '/src/assets/images/project-6.jpg',
-      title: 'vCard Personal portfolio',
-      tags: ['Web-design', 'Development'],
-      projectLink: 'https://github.com/Krishkumarchhabra25'
-    },
-  ];
+  {
+    imgSrc: project1,
+    title: 'Full stack music app',
+    tags: ['API', 'MVC', 'Development'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
+  },
+  {
+    imgSrc: project2,
+    title: 'Free stock photo app',
+    tags: ['API', 'SPA'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
+  },
+  {
+    imgSrc: project3,
+    title: 'Recipe app',
+    tags: ['Development', 'API'],
+    projectLink: ''
+  },
+  {
+    imgSrc: project4,
+    title: 'Real state website',
+    tags: ['Web-design', 'Development'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
+  },
+  {
+    imgSrc: project5,
+    title: 'eCommerce website',
+    tags: ['eCommerce', 'Development'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
+  },
+  {
+    imgSrc: project6,
+    title: 'vCard Personal portfolio',
+    tags: ['Web-design', 'Development'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
+  },
+];
+
 
 const Work = () => {
   return (
