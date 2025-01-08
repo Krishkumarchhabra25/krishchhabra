07cc2d0cca9d3e477914092484f5402dcc10141b
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2025-01-08 17:01:09 +0530

    All updates done

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,48 +1,69 @@
 import ProjectCard from "./ProjectCard";
 
-import project1 from '../assets/images/project-1.jpg';
-import project2 from '../assets/images/project-2.jpg';
-import project3 from '../assets/images/project-3.jpg';
-import project4 from '../assets/images/project-4.jpg';
-import project5 from '../assets/images/project-5.jpg';
-import project6 from '../assets/images/project-6.jpg';
+import project1 from '../assets/images/arohimediaproject.png';
+import project2 from '../assets/images/clickezy.png';
+import project3 from '../assets/images/Screenshot 2025-01-08 154602.png';
+import project4 from '../assets/images/odishataikondoweb.png';
+import project5 from '../assets/images/Screenshot 2025-01-08 160630.png';
+import project6 from '../assets/images/fullstackecommerce.png';
+import project7 from '../assets/images/wemakedevsclone.png';
+import project8 from '../assets/images/eccomerce.png';
+import project9 from '../assets/images/spotifyclone.png';
 
 const works = [
   {
     imgSrc: project1,
-    title: 'Full stack music app',
-    tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://github.com/Krishkumarchhabra25'
+    title: 'Aarohi media App',
+    tags: ['React', 'gsap', 'Tailwind css' , 'Redux'],
+    projectLink: 'https://aarohimediatest.netlify.app/#/'
   },
   {
     imgSrc: project2,
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://github.com/Krishkumarchhabra25'
+    title: 'Clickezy Web App',
+    tags: ['Nextjs', 'Tailwindcss' , 'Redux'],
+    projectLink: 'https://clickezy-frontend.vercel.app/'
   },
   {
     imgSrc: project3,
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
+    title: 'Heads App',
+    tags: ['Swift', 'Swift Ui' , 'Avf Foundation' , 'GeoLocation'],
+    projectLink: 'https://github.com/Krishkumarchhabra25'
   },
   {
     imgSrc: project4,
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/Krishkumarchhabra25'
+    title: 'odishataekwondo',
+    tags: ['React', 'Redux' ,'Tailwindcss'],
+    projectLink: 'https://odishataekwondoassociation.com/#/'
   },
   {
     imgSrc: project5,
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
+    title: 'Wallpaper App',
+    tags: ['React Native', 'Download feature'],
     projectLink: 'https://github.com/Krishkumarchhabra25'
   },
-  {
+  { 
     imgSrc: project6,
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/Krishkumarchhabra25'
+    title: 'FullStack Projets',
+    tags: ['React js', 'Tailwind css' , 'Prisma' , 'mongoDB'],
+    projectLink: 'https://github.com/Krishkumarchhabra25/Pratice-EcommerceWebsite'
+  },
+  {
+    imgSrc: project7,
+    title: 'wemakedevlopers',
+    tags: ['React js', 'Development'],
+    projectLink: 'https://github.com/Krishkumarchhabra25/wemakedevlopers'
+  },
+  {
+    imgSrc: project8,
+    title: 'Eccomerce',
+    tags: ['React js', 'MongoDb' , 'Prisma' , 'SupaBase'],
+    projectLink: 'https://shopping-shop-krish.vercel.app/'
+  },
+  {
+    imgSrc: project9,
+    title: 'Spotify Clone',
+    tags: ['React js', 'MongoDb' , 'Prisma' , 'SupaBase'],
+    projectLink: 'https://spotifymusic-krish.vercel.app/'
   },
 ];
 
